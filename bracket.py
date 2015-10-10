0ea4c930869699e3272aa4e6cddbd93226f48f70
Marcin Migda <marcin700@gmail.com>
2015-10-10 22:59:00 +0200

    Dodawanie groupBoxów na podstawie ilości drużyn

---
+++ bracket.py
@@ -0,0 +1,22 @@
+﻿from PyQt4.QtGui import QGroupBox
+
+class Bracket(object):
+
+    def init_bracket(self, ui, team_number):
+
+        result = 0
+        number_of_groupbox = 0
+
+        if team_number != 0:
+            while (result != 1):
+                if team_number % 2 != 0:
+                    team_number = team_number + 1
+                number_of_groupbox += 1
+                print number_of_groupbox
+                result = team_number / 2
+                team_number = result
+                print result
+
+        for x in range(number_of_groupbox):
+            groupBox = QGroupBox()
+            ui.horizontalLayout.addWidget(groupBox)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from PyQt4 import QtCore, QtGui, Qt
+﻿from PyQt4 import QtCore, QtGui, Qt
 from PyQt4.QtGui import QMessageBox
 import sys
 
 from ui_files import Ui_MainWindow
+from bracket import Bracket
 
 class TournamentManager(QtGui.QMainWindow):
     def __init__(self, *args):
@@ -10,6 +11,19 @@ class TournamentManager(QtGui.QMainWindow):
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        
+        self.init_vars()
+
+        self.bracket = Bracket()
+        self.create_bracket()
+
+    def init_vars(self):
+        self.number_of_teams = 0
+
+        
+    def create_bracket(self):
+        ui = self.ui
+        self.bracket.init_bracket(ui, self.number_of_teams)
 
 def main():
     app = QtGui.QApplication(sys.argv)
